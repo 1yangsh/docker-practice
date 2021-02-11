b26b190cb530eeac5a1b3343bcdf6ce285c302b2
1yangsh <ysh410@gmail.com>
2021-02-11 19:46:37 +0900

    practice docker container

--- django_project/mysite/mysite/settings.py
+++ django_project/mysite/mysite/settings.py
@@ -82,9 +82,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'mysql_server',
+        'PORT': '3306'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
